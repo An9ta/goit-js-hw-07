64d3a2bba1c1d1b4b993e7540db562cfb6554c2f
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-01 01:36:38 +0200

    3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+function createGallery(listImage) {
+  const gallery = listImage
+    .map(
+      (image) =>
+        `<li> <div><a href=""><img class="gallery__image" 
+        src=${image.preview} data-source=${image.original} 
+        alt="${image.description}"></a></div> </li>`
+    )
+    .join("");
+  gallery.insertAdjacentHTML("beforeend", gallery);
+}
+createGallery(images);
