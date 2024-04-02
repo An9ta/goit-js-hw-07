7370e597582c054b358a274185abe91ce05ac5f5
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-02 22:51:55 +0200

    7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = galleryItems.map(({ original, preview, description }) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = preview;
+  galleryImage.setAttribute("title", description);
+  galleryImage.alt = description;
+  galleryLink.append(galleryImage);
+  return galleryLink;
+});
+
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
